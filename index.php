2f77ec5fd8675974fc4b900d57d30379bc9e9f25
Syfur Rahaman Shohag <SYFUR.SHOHAG@icddrb.net>
2024-12-18 16:46:13 +0600

    Dataset Modification

--- index.php
+++ index.php
@@ -6,11 +6,11 @@ include_once "Class/Node.php";
 include_once "Class/DecissionTree.php";
 
 echo "Using training data to generate Decision Tree...<br>";
-$dec_tree = new DecisionTree('data/weather.csv', 0);
+$dec_tree = new DecisionTree('data/train.csv', 0);
 
 echo "Decision tree using ID3:<br>";
 $dec_tree->display();
 
 echo "<br>Prediction on new data set:";
-$dec_tree->predict_outcome('data/input_weather.csv');
+$dec_tree->predict_outcome('data/test.csv');
 ?>
