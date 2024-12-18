cfa7cc8a351090ce73ebbdb5ee0018926808eea7
Syfur Rahaman Shohag <SYFUR.SHOHAG@icddrb.net>
2024-12-18 15:59:22 +0600

    First Commit

---
+++ Class/Tree.php
@@ -0,0 +1,14 @@
+<?php
+class Tree {
+	protected	$root;
+	private		$currentNode;
+	
+	public function __construct($root) {
+		$this->root = $root;
+	}
+	
+	public function display() {
+		$this->root->display(0);
+	}
+}
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+error_reporting(E_ALL | E_STRICT);
+
+include_once "Class/Tree.php";
+include_once "Class/Node.php";
+include_once "Class/DecissionTree.php";
+
+echo "Using training data to generate Decision Tree...<br>";
+$dec_tree = new DecisionTree('data/weather.csv', 0);
+
+echo "Decision tree using ID3:<br>";
+$dec_tree->display();
+
+echo "<br>Prediction on new data set:";
+$dec_tree->predict_outcome('data/input_weather.csv');
+?>
